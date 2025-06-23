86491d3d89d14c721ec853282fe588a1a8e02c07
Rafael <rafael.felix.moraes@gmail.com>
2025-06-22 23:46:12 -0300

    Refactor acessando doc

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ pizzaJson.map((item, index)=>{
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
     //preencher as informações em pizzaitem
 
-    
+    document.querySelector('.pizza-area').append( pizzaItem);
 });
